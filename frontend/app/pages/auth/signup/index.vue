cae7caed4b90d1dd0b224db6d1625971054c60a4
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-07-17 12:00:47 +0800

    updated signup UX

--- frontend/app/pages/auth/signup/index.vue
+++ frontend/app/pages/auth/signup/index.vue
@@ -25,15 +25,16 @@ interface SignUpError {
 }
 async function signUpHandler() {
 	signUpError.value = false;
+	signUpErrorMessages.value = [];
 	if (!signUpCredentialsValidation.value.username || !signUpCredentialsValidation.value.email || !signUpCredentialsValidation.value.password || signUpCredentials.password !== signUpCredentials.confirmPassword) {
 		signUpError.value = true;
-		signUpErrorMessage.value = 'Please fill in all fields correctly.';
+		signUpErrorMessages.value = ['Please fill in all fields correctly.'];
 		return;
 	}
 	try {
 		await signUp(signUpCredentials, { preventLoginFlow: true, callGetSession: false })
 		signUpError.value = false;
-		signUpErrorMessage.value = '';
+		signUpErrorMessages.value = [];
 		toast.add({
 			severity: 'success',
 			detail: 'Sign up successful! Please check your email to verify your account.',
@@ -48,19 +49,19 @@ async function signUpHandler() {
 		const err = error as SignUpError;
 		if (!err || !err.data || !err.data.errors) {
 			signUpError.value = true;
-			signUpErrorMessage.value = 'An unexpected error occurred. Please try again later.';
+			signUpErrorMessages.value = ['An unexpected error occurred. Please try again later.'];
 			return;
 		}
 		const errorDetails = err.data.errors;
 		const parsedErrors = errorDetails
-			? errorDetails.map((err) => err.message).join(', ')
-			: 'An error occurred during sign up.';
+			? errorDetails.map(err => err.message)
+			: ['An error occurred during sign up.'];
 		signUpError.value = true;
-		signUpErrorMessage.value = parsedErrors;
+		signUpErrorMessages.value = parsedErrors;
 	}
 }
 const signUpError = ref(false);
-const signUpErrorMessage = ref('');
+const signUpErrorMessages = ref<string[]>([]);
 const showPassword = ref(false);
 const toast = useToast();
 </script>
@@ -73,31 +74,37 @@ const toast = useToast();
 					<h3 class="mt-3 text-xl font-medium text-center text-gray-600">Sign up</h3>
 					<form @submit.prevent="signUpHandler">
 						<div class="w-full mt-4">
-							<input v-model="signUpCredentials.username"
+							<input
+v-model="signUpCredentials.username"
 								class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-orange-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-orange-300"
 								type="text" placeholder="Username" aria-label="Username" required>
 						</div>
 						<div class="w-full mt-4">
-							<input v-model="signUpCredentials.email"
+							<input
+v-model="signUpCredentials.email"
 								class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-orange-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-orange-300"
 								type="email" placeholder="Email Address" aria-label="Email Address" required>
 						</div>
 
 						<div class="w-full mt-4">
-							<input v-model="signUpCredentials.password"
+							<input
+v-model="signUpCredentials.password"
 								class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-orange-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-orange-300"
 								:type="showPassword ? 'text' : 'password'" placeholder="Password" aria-label="Password" required>
 						</div>
 						<div class="w-full mt-4 relative">
-							<input v-model="signUpCredentials.confirmPassword"
+							<input
+v-model="signUpCredentials.confirmPassword"
 								class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-orange-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-orange-300"
 								:type="showPassword ? 'text' : 'password'" placeholder="Confirm Password" aria-label="Confirm Password"
 								required>
-							<button v-if="!showPassword" type="button" class="absolute right-4 top-3 hover:text-gray-500 duration-100"
+							<button
+v-if="!showPassword" type="button" class="absolute right-4 top-3 hover:text-gray-500 duration-100"
 								@click="showPassword = true">
 								<Icon name="weui:eyes-on-filled" class="text-xl" />
 							</button>
-							<button v-else type="button" class="absolute right-4 top-3 hover:text-gray-500 duration-100"
+							<button
+v-else type="button" class="absolute right-4 top-3 hover:text-gray-500 duration-100"
 								@click="showPassword = false">
 								<Icon name="weui:eyes-off-filled" class="text-xl" />
 							</button>
@@ -109,7 +116,9 @@ const toast = useToast();
 							</button>
 						</div>
 						<div v-if="signUpError">
-							<p class="mt-2 text-sm text-red-500 text-center">{{ signUpErrorMessage }}</p>
+							<p v-for="error, i in signUpErrorMessages" :key="i" class="mt-1 text-sm text-red-500 text-center">
+								{{ error }}
+							</p>
 						</div>
 					</form>
 				</div>
