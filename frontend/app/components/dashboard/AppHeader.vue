0a4ba9ef8c895a2093f3e0781bed2ae261331f3b
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-07-20 19:56:57 +0800

    moved some components to 'dashboard' folder

---
+++ frontend/app/components/dashboard/AppHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { signOut } = useAuth();
+async function signOutHandler() {
+	await signOut({ external: true });
+}
+</script>
+<template>
+	<header class="w-full">
+		<nav class="relative bg-white">
+			<div class="px-12 py-4 mx-auto flex justify-between items-center">
+				<div class="flex items-center justify-between">
+					<NuxtLink to="/app" class="flex gap-2">
+						<h1 class="text-gray-900 font-bold text-xl">webstandr</h1>
+						<div class="p-2 bg-orange-500 rounded-full" />
+					</NuxtLink>
+				</div>
+				<div>
+					<Button @click="signOutHandler">
+						<Icon name="streamline:logout-1" />
+						Logout
+					</Button>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
