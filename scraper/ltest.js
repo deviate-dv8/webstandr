6973fc3e6ea02e802d4182139ea5238a11608548
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-07-27 18:42:17 +0800

    added k6 testing

---
+++ scraper/ltest.js
@@ -0,0 +1,22 @@
+import http from "k6/http";
+import faker from "k6/x/faker";
+export const options = {
+  vus: 250,
+  duration: "60s",
+};
+const provider = ["google", "bing", "yahoo", "duckduckgo"];
+export default function () {
+  const url = "http://localhost:3000/api/serp/search";
+  const payload = JSON.stringify({
+    query: faker.person.firstName(),
+    provider: "yahoo",
+    // provider: provider[Math.floor(Math.random() * provider.length)],
+  });
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = http.post(url, payload, params);
+  console.log(`Response status: ${response.status}`);
+}
