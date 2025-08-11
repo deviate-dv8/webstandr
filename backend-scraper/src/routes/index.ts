2f6d165287b6fac6e06f4e06be876b81fdd3a10f
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-12 06:52:49 +0800

    removed backend-scraper and added serpfree as a submodule

--- backend-scraper/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import serpController from "../controllers/serp-controller";
-const router = Router();
-
-router.use("/serp", serpController);
-
-export default router;
