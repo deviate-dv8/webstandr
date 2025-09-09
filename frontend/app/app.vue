083b311fc7e42a7fe7a9cda52e7b0f6bbe738074
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-09-09 23:38:42 +0800

    fix: toast responsiveness

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,7 +1,7 @@
 <template>
+	<Toast class="z-[100]" />
+	<Toast class="z-[100]" position="top-center" group="app" />
 	<NuxtLayout>
-		<Toast class="z-[100]" />
-		<Toast class="z-[100]" position="top-center" group="app" />
 		<NuxtPage />
 	</NuxtLayout>
 </template>
