1daa5de47861aa67e56835855c26c64a46b827eb
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-09-14 07:21:28 +0800

    feat: added website name in prompts index frontend

--- frontend/app/components/PromptTable.vue
+++ frontend/app/components/PromptTable.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
-defineProps({
+import type { Website } from '~/types/website'
+
+interface PromptsExtended extends Prompt {
+	website: Website
+}
+const props = defineProps({
 	prompts: {
-		type: Array as () => Prompt[],
+		type: Array as () => PromptsExtended[],
 		required: true
 	},
 	deleteFunction: {
@@ -13,16 +18,23 @@ async function handlePromptRedirect(e: { data: Prompt }) {
 	const prompt = e.data
 	await navigateTo(`/app/prompts/${prompt.id}`)
 }
+
 const columns = [
+	(props.prompts.some(p => p.website)) ? { field: 'website.name', header: 'Website' } : null,
+	{ field: 'name', header: 'Name' },
+	{ field: 'query', header: 'Query' },
+	{ field: 'provider', header: 'Provider' },
+	{ field: 'schedule', header: 'Schedule' },
 	{ field: 'name', header: 'Name' },
 	{ field: 'query', header: 'Query' },
 	{ field: 'provider', header: 'Provider' },
 	{ field: 'schedule', header: 'Schedule' },
 ]
+
 </script>
 <template>
 	<DataTable :value="prompts" :row-hover="true" @row-click="handlePromptRedirect">
-		<Column v-for="column of columns" :key="column.field" :field="column.field" :header="column.header" />
+		<Column v-for="column of columns" :key="column?.field" :field="column?.field" :header="column?.header" />
 		<Column header="Actions">
 			<template #body="slotProps">
 				<Button :id="slotProps.data.id" icon="pi pi-trash" severity="danger" @click="deleteFunction as any" />
